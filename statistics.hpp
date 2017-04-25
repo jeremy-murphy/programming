f29d575549686caf69d4c23dbfda7f560d18cf87
Jeremy W. Murphy <jeremy.william.murphy@gmail.com>
2017-04-25 23:35:14 +1000

    Scope the x and y means.

--- statistics.hpp
+++ statistics.hpp
@@ -18,11 +18,14 @@ namespace jwm
         using namespace std;
         auto const n = distance(x1, xn);
         auto const yn = y1 + n;
-        auto x_mean = accumulate(next(x1), xn, *x1) / n,
-             y_mean = accumulate(next(y1), yn, *y1) / n;
-             
-        transform(x1, xn, x1, bind(minus<>(), placeholders::_1, x_mean));
-        transform(y1, yn, y1, bind(minus<>(), placeholders::_1, y_mean));
+        
+        {
+            auto x_mean = accumulate(next(x1), xn, *x1) / n,
+                 y_mean = accumulate(next(y1), yn, *y1) / n;
+                
+            transform(x1, xn, x1, bind(minus<>(), placeholders::_1, x_mean));
+            transform(y1, yn, y1, bind(minus<>(), placeholders::_1, y_mean));
+        }
         
         auto const numer = inner_product(next(x1), xn, next(y1), *x1 * *y1);
         auto const x_ss = inner_product(next(x1), xn, next(x1), *x1 * *x1),
