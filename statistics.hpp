152dd06a3ce8bc1c7d9410ceb95b7ccb215cf8a8
Jeremy W. Murphy <jeremy.william.murphy@gmail.com>
2017-04-26 00:36:14 +1000

    Abstract out the calculation of the mean.

--- statistics.hpp
+++ statistics.hpp
@@ -23,22 +23,17 @@ namespace jwm
         return std::inner_product(std::next(f0), l0, std::next(f1), *f0 * *f1);
     }
     
+    
     // I'm using x1, xn, etc just to mimic the stats lingo.
-    template <typename I, typename J>
-    auto Pearson_correlation_coefficient_a(I x1, I xn, J y1) -> decltype(auto)
+    template <typename I, typename J, typename F>
+    auto Pearson_correlation_coefficient_a(I x1, I xn, J y1, F mean) -> decltype(auto)
     {
         assert(x1 != xn);
         using namespace std;
-        auto const n = distance(x1, xn);
-        auto const yn = y1 + n;
+        auto const yn = y1 + distance(x1, xn);
         
-        {
-            auto x_mean = accumulate_nonempty(x1, xn) / n,
-                 y_mean = accumulate_nonempty(y1, yn) / n;
-                
-            transform(x1, xn, x1, bind(minus<>(), placeholders::_1, x_mean));
-            transform(y1, yn, y1, bind(minus<>(), placeholders::_1, y_mean));
-        }
+        transform(x1, xn, x1, bind(minus<>(), placeholders::_1, mean(x1, xn)));
+        transform(y1, yn, y1, bind(minus<>(), placeholders::_1, mean(y1, yn)));
         
         auto const numer = inner_product_nonempty(x1, xn, y1);
         auto const x_ss = inner_product_nonempty(x1, xn, x1),
@@ -47,6 +42,15 @@ namespace jwm
         assert(denom);
         return numer / denom;
     }
+
+    
+    template <typename I, typename J>
+    auto Pearson_correlation_coefficient_a(I x1, I xn, J y1) -> decltype(auto)
+    {
+        auto const mean = [&](I f, I l){ return accumulate_nonempty(f, l) / std::distance(f, l); }; // or Boost accumulator
+        return Pearson_correlation_coefficient_a(x1, xn, y1, mean);
+    }
+    
 }
 
 #endif
