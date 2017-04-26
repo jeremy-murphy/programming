1e224d76adb8ffb478b3d4d4f47a248af3183f2b
Jeremy W. Murphy <jeremy.william.murphy@gmail.com>
2017-04-26 22:19:46 +1000

    Add a test case from MathWorks website.

--- test/test_statistics.cpp
+++ test/test_statistics.cpp
@@ -5,8 +5,18 @@
 using namespace jwm;
 using namespace std;
 
+
 TEST(Pearson_correlation_coefficient, singleton)
 {
     std::vector<double> x(1), y(x);
     double r = Pearson_correlation_coefficient_a(begin(x), end(x), begin(y));
+}
+
+TEST(Pearson_correlation_coefficient, MathWorksEx1)
+{
+    vector<double> x = {7, 33/7, 3, 5, 2},
+                   y = {3, 5, 1, 7, 2};
+    
+    auto r = Pearson_correlation_coefficient_a(begin(x), end(x), begin(y));
+    ASSERT_EQ(0.4514558056, r);
 }
