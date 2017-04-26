2a5f922c567ce641c811e42fbab7b6e21bb37ed7
Jeremy W. Murphy <jeremy.william.murphy@gmail.com>
2017-04-26 22:19:16 +1000

    Pass mean values instead of function to calculate the mean.

--- statistics.hpp
+++ statistics.hpp
@@ -25,21 +25,20 @@ namespace jwm
     
     
     // I'm using x1, xn, etc just to mimic the stats lingo.
-    template <typename I, typename J, typename F>
-    auto Pearson_correlation_coefficient_a(I x1, I xn, J y1, F mean) -> decltype(auto)
+    template <typename I, typename J, typename T>
+    auto Pearson_correlation_coefficient_a(I x1, I xn, J y1, T mean_x, T mean_y) -> decltype(auto)
     {
         assert(x1 != xn);
         using namespace std;
         auto const yn = y1 + distance(x1, xn);
         
-        transform(x1, xn, x1, bind(minus<>(), placeholders::_1, mean(x1, xn)));
-        transform(y1, yn, y1, bind(minus<>(), placeholders::_1, mean(y1, yn)));
+        transform(x1, xn, x1, bind(minus<>(), placeholders::_1, mean_x));
+        transform(y1, yn, y1, bind(minus<>(), placeholders::_1, mean_y));
         
         auto const numer = inner_product_nonempty(x1, xn, y1);
         auto const x_ss = inner_product_nonempty(x1, xn, x1),
                    y_ss = inner_product_nonempty(y1, yn, y1);
         auto const denom = sqrt(x_ss) * sqrt(y_ss);
-        assert(denom);
         return numer / denom;
     }
 
@@ -47,10 +46,15 @@ namespace jwm
     template <typename I, typename J>
     auto Pearson_correlation_coefficient_a(I x1, I xn, J y1) -> decltype(auto)
     {
-        auto const mean = [&](I f, I l){ return accumulate_nonempty(f, l) / std::distance(f, l); }; // or Boost accumulator
-        return Pearson_correlation_coefficient_a(x1, xn, y1, mean);
-    }
-    
+        assert(x1 != xn);
+        auto const mean = [&](I f, I l)
+        {
+            return accumulate_nonempty(f, l) / std::distance(f, l);
+        }; // or Boost accumulator
+        auto const mean_x = mean(x1, xn), 
+                   mean_y = mean(y1, y1 + distance(x1, xn));
+        return Pearson_correlation_coefficient_a(x1, xn, y1, mean_x, mean_y);
+    }    
 }
 
 #endif
