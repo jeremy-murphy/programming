6f87b6f1a6c8dc2ca5726fceda570ab7d0527624
Jeremy W. Murphy <jeremy.william.murphy@gmail.com>
2017-04-30 22:18:28 +1000

    Add bind1st and bind2nd.

--- functional.hpp
+++ functional.hpp
@@ -1,6 +1,7 @@
 #ifndef JWM_FUNCTIONAL_HPP
 #define JWM_FUNCTIONAL_HPP
 
+#include <functional>
 #include <utility>
 
 namespace jwm
@@ -14,6 +15,20 @@ struct identity
         return std::forward<T>(x);
     }
 };
+
+
+template <typename F, typename T>
+auto bind1st(F &&f, T &&x)
+{
+    return std::bind(std::forward<F>(f), std::forward<T>(x), std::placeholders::_1);
+}
+
+
+template <typename F, typename T>
+auto bind2nd(F &&f, T &&x)
+{
+    return std::bind(std::forward<F>(f), std::placeholders::_1, std::forward<T>(x));
+}
 
 }
 
