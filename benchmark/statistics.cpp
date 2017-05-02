79341509ab3c201d79510abd479b17265d4ab30a
Jeremy W. Murphy <jeremy.william.murphy@gmail.com>
2017-05-02 23:53:33 +1000

    Add statistics benchmark.

---
+++ benchmark/statistics.cpp
@@ -0,0 +1,17 @@
+#include <benchmark/benchmark.h>
+
+#include "../statistics.hpp"
+
+static void BM_Pearson_correlation(benchmark::State &s)
+{
+    std::vector<double> x(s.range(0)), y(s.range(0));
+
+    while (s.KeepRunning())
+    {
+        benchmark::DoNotOptimize(jwm::Pearson_correlation_coefficient(begin(x), end(x), begin(y)));
+    }
+}
+
+BENCHMARK(BM_Pearson_correlation)->Range(8, 8<<20);
+
+BENCHMARK_MAIN();
